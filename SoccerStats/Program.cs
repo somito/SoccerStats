af76147d813ad0be1d4a19e04bc0e80d06baafee
hu3soan0 <hu3soan0@HU03ON20138>
2016-10-05 13:17:34 +0200

    Add project files.

---
+++ SoccerStats/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SoccerStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = ("Z:\\Projects\\SoccerStats\\SoccerStats");
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadFile(fileName);
+            string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach(var line in fileLines)
+            {
+                Console.WriteLine(line);
+            }
+            
+        }
+
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
+
