921f77bc3af0d4195a312e2011aa9f1de2b17daf
Somito <somodian@hotmail.com>
2016-10-07 15:12:02 +0200

    Added a json data file player.json, that contains data about soccer players. Added a deserializer method DeserializePlayers to store the data in object of Players class.

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace SoccerStats
 {
@@ -15,9 +16,14 @@ namespace SoccerStats
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
             var fileContents = ReadSoccerResults(fileName);
+            fileName = Path.Combine(directory.FullName, "players.json");
+            var players = DeserializePlayers(fileName);
 
-            
-            
+            foreach (var player in players)
+            {
+                Console.WriteLine(player.second_name);
+            }
+
         }
 
 
@@ -86,6 +92,19 @@ namespace SoccerStats
                 }
             }
             return soccerResults;
+        }
+
+        public static List<Player> DeserializePlayers(string fileName)
+        {
+            var players = new List<Player>();
+            var serializer = new JsonSerializer();
+            using (var reader = new StreamReader(fileName))
+            using (var jsonReader = new JsonTextReader(reader))
+            {
+                players = serializer.Deserialize<List<Player>>(jsonReader);
+            }
+            return players;
+
         }
     }
 }
